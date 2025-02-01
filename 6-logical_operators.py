229fdbff0334ba2d0c3c369a707ec383d15fe822
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-02-01 22:38:31 +0500

    logical-operators

---
+++ 6-logical_operators.py
@@ -0,0 +1,39 @@
+#or
+#and
+#not
+
+temp = 25
+is_raining = False
+
+if temp > 35 or temp < 0 or is_raining:
+    print("The outdoor event is cancelled")
+else:
+    print("The outdoor event is sitll scheduled!")
+
+
+temp = -5
+is_sunny = False
+
+if temp >= 28 and is_sunny:
+    print("Its Hot Outside")
+    print("Its Sunny")
+
+elif temp <=0 and is_sunny:
+    print("Its Cold Outside")
+    print("Its Sunny")
+
+elif 28 > temp > 0 and is_sunny:
+    print("Its Warm Outside")
+    print("Its Sunny")
+
+elif temp >= 28 and not is_sunny:
+    print("Its Hot Outside")
+    print("Its Cloudy")
+
+elif temp <=0 and not is_sunny:
+    print("Its Cold Outside")
+    print("Its Cloudy")
+
+elif 28 > temp > 0 and not is_sunny:
+    print("Its Warm Outside")
+    print("Its Cloudy")
