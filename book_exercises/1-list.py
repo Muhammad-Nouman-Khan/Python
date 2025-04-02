3fbee859ed2fe79ae002823104eae45ebdbe5c4c
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-04-02 18:36:39 +0500

    update

---
+++ book_exercises/1-list.py
@@ -0,0 +1,12 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+ print(f"{magician.title()}, that was a great trick!")
+ print(f"I can't wait to see your next trick, {magician.title()}.\n")
+print("Thank you, everyone. That was a great magic show!")
+
+#Using range() to Make a List of Numbers
+numbers = list(range(1, 6))
+print(numbers)
+
+even_numbers = list(range(2, 11, 2))
+print(even_numbers)
