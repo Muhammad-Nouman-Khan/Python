624c4787e6ec07b759d9ded30cc9f0cfebaafc4f
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-04-22 13:15:28 +0500

    collections

---
+++ 11-collection.py
@@ -0,0 +1,35 @@
+# collection = single variable used to store multiple values
+# List = [] ordered and changeable. Duplicated OK
+# Set = {} unordered and immutable, but Add/Remove OK. No duplicates
+# Tuple = ()  ordered and unchangeable. Duplicated OK. FASTER
+
+
+#LISTS : 
+fruits = ["apple","orange","banana","coconut"]
+# print(dir(fruits))  #print methods and attributes of the fruits.
+# print(help(fruits)) # description of methods and attributes
+
+fruits.append("pineapple")
+fruits.remove("apple")
+fruits.insert(0,"mango")
+fruits.sort()
+fruits.reverse()
+fruits.clear()
+print(fruits.count("coconut"))
+
+
+#SETS :
+
+brands = {"samsung","apple","oppo","asus"}
+
+#print(brands[0])   # error -> as the sets are unordered.
+brands.add("realme")
+brands.remove("apple")
+brands.pop()  # first element removed, randomly though
+
+
+# TUPLE
+cars = ("mercedes","bmw")
+
+print(cars.index("bmw"))
+print(cars.count("mercedes"))
