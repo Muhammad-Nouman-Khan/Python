c46f1c81510fb9c43facdd567cc9729bac0d9e2a
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-06-15 22:10:01 +0500

    working with lists

--- lists.py
+++ lists.py
@@ -1,52 +1,67 @@
-motorcycles = ['honda','yamaha','suzuki']
-print(motorcycles)
+# motorcycles = ['honda','yamaha','suzuki']
+# print(motorcycles)
 
-print(motorcycles[-1])   # print the last value
+# print(motorcycles[-1])   # print the last value
 
-motorcycles.append('ducati')
-print(motorcycles)
+# motorcycles.append('ducati')
+# print(motorcycles)
 
-motorcycles.insert(0,'BMW')
-print(motorcycles)
+# motorcycles.insert(0,'BMW')
+# print(motorcycles)
 
-del(motorcycles[0])
-print(motorcycles)
+# del(motorcycles[0])
+# print(motorcycles)
 
-popped_motorcyle = motorcycles.pop()   #removes the last
-print(motorcycles)
-print(popped_motorcyle)
+# popped_motorcyle = motorcycles.pop()   #removes the last
+# print(motorcycles)
+# print(popped_motorcyle)
 
-last_owned = popped_motorcyle
-print(f"The last motorcycle I owned was a {last_owned}")
+# last_owned = popped_motorcyle
+# print(f"The last motorcycle I owned was a {last_owned}")
 
-first_owned = motorcycles.pop(0)   #Popping Items from any Position in a List
-print(f"The first motorcycle I owned was a {first_owned}")
-
-
-print(motorcycles)
-
-too_expensive = 'suzuki'
-motorcycles.remove(too_expensive)
-print(motorcycles)
-print(f"\nA {too_expensive.title()} is too expensive for me.")
+# first_owned = motorcycles.pop(0)   #Popping Items from any Position in a List
+# print(f"The first motorcycle I owned was a {first_owned}")
 
 
-cars = ['bmw','audi','toyota','subaru']
-cars.sort()
-print(cars)
-cars.sort(reverse=True)
-print(cars)
+# print(motorcycles)
 
-print(sorted(cars))  #Sorting a List Temporarily with the sorted() Function
-print(cars)
+# too_expensive = 'suzuki'
+# motorcycles.remove(too_expensive)
+# print(motorcycles)
+# print(f"\nA {too_expensive.title()} is too expensive for me.")
 
-cars.reverse()   #Printing a List in Reverse Order
-print(cars)
 
-print(len(cars))
+# cars = ['bmw','audi','toyota','subaru']
+# cars.sort()
+# print(cars)
+# cars.sort(reverse=True)
+# print(cars)
+
+# print(sorted(cars))  #Sorting a List Temporarily with the sorted() Function
+# print(cars)
+
+# cars.reverse()   #Printing a List in Reverse Order
+# print(cars)
+
+# print(len(cars))
 
 
 # WORKING WITH LISTS
 
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick, {magician.title()}.\n")
 
+print("Thank you, everyone. That was a great magic show!\n")
+
+print("****************************")
+
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick!")
+print(f"I can't wait to see your next trick, {magician.title()}.\n")
+
+print("****************************")
+
+#************************************************************
 
