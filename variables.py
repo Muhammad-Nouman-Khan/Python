7cc46af422de29b5dc9c0b6c185cb90b7f5ff0ad
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-06-15 21:16:22 +0500

    variables

---
+++ variables.py
@@ -0,0 +1,24 @@
+name = 'Nouman'
+print(name)
+
+name = 'nouman khan'
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "Nouman"
+last_name = "Khan"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+print(f"Hello, {full_name.title()} !")
+
+print("Languages:\n\t1)Python\n\t2)C++\n\t3)JS")
+
+
+favourite_language = '   python    '
+print(favourite_language)
+print(favourite_language.rstrip())
+print(favourite_language.lstrip())
+print(favourite_language.strip())
+
