80789912f8093b301ed3e23b13fd828a9ab7c662
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-06-15 21:57:43 +0500

    lists

---
+++ lists.py
@@ -0,0 +1,52 @@
+motorcycles = ['honda','yamaha','suzuki']
+print(motorcycles)
+
+print(motorcycles[-1])   # print the last value
+
+motorcycles.append('ducati')
+print(motorcycles)
+
+motorcycles.insert(0,'BMW')
+print(motorcycles)
+
+del(motorcycles[0])
+print(motorcycles)
+
+popped_motorcyle = motorcycles.pop()   #removes the last
+print(motorcycles)
+print(popped_motorcyle)
+
+last_owned = popped_motorcyle
+print(f"The last motorcycle I owned was a {last_owned}")
+
+first_owned = motorcycles.pop(0)   #Popping Items from any Position in a List
+print(f"The first motorcycle I owned was a {first_owned}")
+
+
+print(motorcycles)
+
+too_expensive = 'suzuki'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f"\nA {too_expensive.title()} is too expensive for me.")
+
+
+cars = ['bmw','audi','toyota','subaru']
+cars.sort()
+print(cars)
+cars.sort(reverse=True)
+print(cars)
+
+print(sorted(cars))  #Sorting a List Temporarily with the sorted() Function
+print(cars)
+
+cars.reverse()   #Printing a List in Reverse Order
+print(cars)
+
+print(len(cars))
+
+
+# WORKING WITH LISTS
+
+
+
