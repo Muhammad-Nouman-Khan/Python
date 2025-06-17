e0d7abbfdff441a3a40f786e982c4b1a7078a4cc
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-06-17 23:21:13 +0500

    Dictionary in a Dictionary

--- 4.dictionaries/dictionaries.py
+++ 4.dictionaries/dictionaries.py
@@ -175,3 +175,32 @@ for name,languages in favorite_languages.items():
 
     for language in languages:
         print(f"\t{language.title()}")
+
+
+print("\n*****************************")
+
+# A Dictionary in a Dictionary
+
+users = {
+    
+    'aeinstein' : {
+        'first' : 'albert',
+        'last' : 'einstein',
+        'location' : 'princeton',
+    },
+
+    'mcurie' : {
+        'first' : 'marie',
+        'last' : 'curie',
+        'location' : 'paris',
+    },
+
+}
+
+for username, user_info in users.items():
+    print(f"\nUsername: {username}")
+    full_name = f"{user_info['first']} {user_info['last']}"
+    location = user_info['location']
+    
+    print(f"\tFull name: {full_name.title()}")
+    print(f"\tLocation: {location.title()}")
