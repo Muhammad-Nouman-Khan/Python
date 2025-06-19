1fcf7949ca76c4d9e3ad77aa13827b9781f8e97d
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-06-19 23:33:19 +0500

    while

--- input_while.py
+++ input_while.py
@@ -1,6 +1,33 @@
-height = input("How tall are you, in inches? ")
-height = int(height)
-if height >= 48:
-    print("\nYou're tall enough to ride!")
-else:
-    print("\nYou'll be able to ride when you're a little older.")
+# height = input("How tall are you, in inches? ")
+# height = int(height)
+# if height >= 48:
+#     print("\nYou're tall enough to ride!")
+# else:
+#     print("\nYou'll be able to ride when you're a little older.")
+
+
+# *********************************************
+
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
+
+
+prompt = "\nPlease enter the name of a city you have visited:"
+prompt += "\n(Enter 'quit' when you are finished.) "
+while True:
+    city = input(prompt)
+    if city == 'quit':
+        break
+    else:
+        print(f"I'd love to go to {city.title()}!")
+
+
+
+current_number = 0
+while current_number < 10:
+    current_number += 1
+    if current_number % 2 == 0:
+        continue
+    print(current_number)
