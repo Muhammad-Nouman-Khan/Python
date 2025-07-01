610a0a9be830fd01615cc7eb57564c183688aaa8
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-07-01 14:19:26 +0500

    Add Dog class with methods for sitting and rolling over

---
+++ 7.classes/dog.py
@@ -0,0 +1,18 @@
+class Dog:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        print(f"{self.name} is now sitting")
+
+    def roll_over(self):
+        print(f"{self.name} rolled over!")
+
+
+dog = Dog("name",3)
+
+print(dog.name)
+print(dog.age)
+dog.sit()
+dog.roll_over()
