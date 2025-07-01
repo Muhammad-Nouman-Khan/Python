706ca8cc9fdcbd12f20cdd14f4afed895f73e6d0
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-07-01 15:18:22 +0500

    Add Battery class and integrate with ElectricCar; implement gas tank methods

--- 7.classes/Car.py
+++ 7.classes/Car.py
@@ -1,30 +1,12 @@
-class Dog:
-    def __init__(self,name,age):
-        self.name = name
-        self.age = age
+class Battery:
+    def __init__(self,battery_size = 75):
+        self.battery_size = battery_size
 
-    def sit(self):
-        print(f"{self.name} is now sitting")
+    def describe_battery(self):
+        print(f"This car has a {self.battery_size}-kWh battery.")
 
-    def roll_over(self):
-        print(f"{self.name} rolled over!")
-
-
-dog = Dog("name",3)
-
-print(dog.name)
-print(dog.age)
-dog.sit()
-dog.roll_over()
-# Modifying an attribute's value directly
-dog.name = "Max"
-print(dog.name)
-
-
-# **************************
 
 class Car:
-    
     def __init__(self, make, model, year):
         self.make = make
         self.model = model
@@ -51,14 +33,11 @@ class Car:
         print("Filling the gas tank.")
 
 
-
-#  INHERITANCE 
-
 class ElectricCar(Car):
 
     def __init__(self, make, model, year):
         super().__init__(make, model, year)
-        self.battery_size = 75
+        self.battery = Battery()  # Create an instance of Battery
 
     def describe_battery(self):
         print(f"This car has a {self.battery_size}-kWh battery.")
@@ -67,11 +46,7 @@ class ElectricCar(Car):
     def fill_gas_tank(self):
         print("This car doesn't have a gas tank!")
 
-my_tesla = ElectricCar("tesla","model s",2019)
 
+my_tesla = ElectricCar("tesla", "model s", 2019)
 print(my_tesla.get_descriptive_name())
-
-my_tesla.describe_battery()
-
-my_tesla.fill_gas_tank()
-
+my_tesla.battery.describe_battery()
